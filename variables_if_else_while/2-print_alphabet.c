acb4f7e28a39857f1be9c497992924e7b9053ac8
Ledwaba Lesego <ledwabalesego@example.com>
2025-06-09 12:10:58 +0000

    Add 2-print_alphabet.c: print lowercase alphabet using putchar

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
