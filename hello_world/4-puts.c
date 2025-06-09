7599eadefcf91d2e95db65d3deb6bcfdd1f029c1
Ledwaba Lesego <ledwabalesego@example.com>
2025-06-09 10:23:28 +0000

    Add 4-puts.c with puts and escaped quote

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Prints a specific message using puts
+ *main - Entry point of the program
  *
- * Return: Always 0
+ *Description: Writing my first C program, but I love JavaScript, sad.
+ *
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
