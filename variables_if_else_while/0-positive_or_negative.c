8a635e680a5a259cff27786f703f76e5c325cd3e
Ledwaba Lesego <ledwabalesego@example.com>
2025-06-09 11:59:12 +0000

    Add 0-positive_or_negative.c

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Assigns a random number to n and prints if it's positive, zero or negative
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int n;
+
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+
+    printf("%d is ", n);
+
+    if (n > 0)
+        printf("positive\n");
+    else if (n == 0)
+        printf("zero\n");
+    else
+        printf("negative\n");
+
+    return (0);
+}
