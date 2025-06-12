54d6cd77c3d5bb05254fd1c1b42cc36509278642
Lesego Ledwaba <ledwabalesego199@gmail.com>
2025-06-12 06:02:03 +0000

    Save local changes before sync

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,7 +1,10 @@
+#include <stdio.h>
 #include "main.h"
 
 int main(void)
 {
-    print_alphabet();
-    return (0);
+	printf("%d\n", _isupper('H')); /* Should print 1 */
+	printf("%d\n", _isupper('h')); /* Should print 0 */
+	printf("%d\n", _isupper('0')); /* Should print 0 */
+	return (0);
 }
