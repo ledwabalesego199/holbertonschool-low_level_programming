37758d8960e61f915892ff0d8472d0cbb14e0680
Lesego Ledwaba <ledwabalesego199@gmail.com>
2025-06-12 11:11:40 +0000

    Implement _print_rev_recursion to print string in reverse recursively

---
+++ recursion/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - test _print_rev_recursion
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_print_rev_recursion("Reverse this!");
+	_putchar('\n');
+	return (0);
+}
