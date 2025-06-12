2fa518d1941e46ea3a937ae3496bbde92bf9eb6a
Lesego Ledwaba <ledwabalesego199@gmail.com>
2025-06-12 05:18:55 +0000

    Add function that prints the alphabet in lowercase with only two _putchar calls

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the lowercase alphabet followed by a new line
+ * print_alphabet - prints the alphabet in lowercase followed by a new line.
  */
 void print_alphabet(void)
 {
-	char letter = 'a';
-
-	while (letter <= 'z')
-	{
-		_putchar(letter);
-		letter++;
-	}
-	_putchar('\n');
+    char letter = 'a';
+    while (letter <= 'z')
+    {
+        _putchar(letter);
+        letter++;
+    }
+    _putchar('\n');
 }
