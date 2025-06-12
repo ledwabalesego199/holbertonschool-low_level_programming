a77c2dd4f38e414b0259413cb959d47c4e43c738
Lesego Ledwaba <ledwabalesego199@gmail.com>
2025-06-12 09:11:13 +0000

    Add function puts2 to print every other character of a string

--- pointers_arrays_strings/_putchar.c
+++ pointers_arrays_strings/_putchar.c
@@ -4,7 +4,7 @@
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * Return: On success 1. On error, -1 is returned.
+ * Return: On success 1, or -1 on error.
  */
 int _putchar(char c)
 {
--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -1,17 +1,13 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - test the rev_string function
+ * main - check the code for puts2
  *
  * Return: Always 0
  */
 int main(void)
 {
-	char str[] = "Holberton";
-
-	printf("Before: %s\n", str);
-	rev_string(str);
-	printf("After: %s\n", str);
+	char *str = "0123456789";
+	puts2(str);
 	return (0);
 }
