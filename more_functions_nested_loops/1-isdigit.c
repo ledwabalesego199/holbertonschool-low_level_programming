f9b32dbd938b287d05e1463a0fa682064f7c9671
Lesego Ledwaba <ledwabalesego199@gmail.com>
2025-06-12 06:39:08 +0000

    Add _isdigit function

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,7 @@
  * _isdigit - checks for a digit (0 through 9)
  * @c: the character to check
  *
- * Return: 1 if digit, 0 otherwise
+ * Return: 1 if c is a digit, 0 otherwise
  */
 int _isdigit(int c)
 {
