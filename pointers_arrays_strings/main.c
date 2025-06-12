ace878fcaac36feb8fe0f877a9b50e803ca0a64d
Lesego Ledwaba <ledwabalesego199@gmail.com>
2025-06-12 08:55:49 +0000

    Add _puts function to print a string followed by a newline

--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
 #include "main.h"
 
+/**
+ * main - test the _puts function
+ * Return: 0
+ */
 int main(void)
 {
-	char *str = "Hello, Holberton!";
-	int length;
-
-	length = _strlen(str);
-	printf("Length of '%s' is %d\n", str, length);
-
-	return (0);
+    _puts("Hello, Holberton!");
+    return (0);
 }
