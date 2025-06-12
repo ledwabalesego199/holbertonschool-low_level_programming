2a38f28d168a772f5ec5f51ac74f8f0e47df6498
Lesego Ledwaba <ledwabalesego199@gmail.com>
2025-06-12 05:25:25 +0000

    Add Betty-compliant function to print the alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
+#include <unistd.h>
 #include "main.h"
 
 /**
- * print_alphabet - prints the alphabet in lowercase followed by a new line.
+ * print_alphabet - Entry point
+ * Return: 0
  */
+
 void print_alphabet(void)
 {
-    char letter = 'a';
-    while (letter <= 'z')
-    {
-        _putchar(letter);
-        letter++;
-    }
-    _putchar('\n');
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
 }
