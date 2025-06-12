de0ad826de040ded9a53be3c6fce7778c8159284
Lesego Ledwaba <ledwabalesego199@gmail.com>
2025-06-12 08:30:44 +0000

    Add _strlen function to return string length with tests

--- main.c
+++ main.c
@@ -3,9 +3,11 @@
 
 int main(void)
 {
-	printf("%d\n", _isdigit('5'));  /* Should print 1 */
-	printf("%d\n", _isdigit('a'));  /* Should print 0 */
-	printf("%d\n", _isdigit('9'));  /* Should print 1 */
-	printf("%d\n", _isdigit('/'));  /* Should print 0 */
+	char *str = "Hello, Holberton!";
+	int length;
+
+	length = _strlen(str);
+	printf("Length of '%s' is %d\n", str, length);
+
 	return (0);
 }
