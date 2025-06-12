20f1acafdc90f17d46539fe2c4bd1e12cd07c133
Lesego Ledwaba <ledwabalesego199@gmail.com>
2025-06-12 06:35:16 +0000

    Commit before syncing with remote

--- more_functions_nested_loops/main.c
+++ more_functions_nested_loops/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-#include "main.h"
+
+int _isdigit(int c); // Function prototype
 
 int main(void)
 {
-	printf("%d\n", _isupper('H')); /* Should print 1 */
-	printf("%d\n", _isupper('h')); /* Should print 0 */
-	printf("%d\n", _isupper('0')); /* Should print 0 */
-	return (0);
+	printf("%d\n", _isdigit('5'));  // Should print 1
+	printf("%d\n", _isdigit('a'));  // Should print 0
+	printf("%d\n", _isdigit('9'));  // Should print 1
+	printf("%d\n", _isdigit('*'));  // Should print 0
+	return 0;
 }
