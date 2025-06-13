a5db9944ae1f5a3d5ffc0f3fb1cd8c212e29a768
Lesego Ledwaba <ledwabalesego199@gmail.com>
2025-06-13 08:44:01 +0000

    Add init_dog function with proper Betty style and documentation

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,17 +1,20 @@
-#include <stddef.h>
 #include "dog.h"
+#include <stddef.h>
 
 /**
- * init_dog - initialize a variable of type struct dog
+ * init_dog - initializes a variable of type struct dog
  * @d: pointer to struct dog to initialize
- * @name: name to assign
- * @age: age to assign
- * @owner: owner to assign
+ * @name: name to assign to dog
+ * @age: age to assign to dog
+ * @owner: owner to assign to dog
+ *
+ * Description: Initializes the members of a dog struct pointed by d.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
     if (d == NULL)
         return;
+
     d->name = name;
     d->age = age;
     d->owner = owner;
