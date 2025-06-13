23c876c8e54cb330be444cfaae2fea8ebd738861
Nonhle_LSG <ledwabalesego199@gmail.com>
2025-06-13 13:01:02 +0200

    Update dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,26 +2,30 @@
 #define DOG_H
 
 /**
- * struct dog - structure for a dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
  *
- * Description: This struct stores basic information about a dog.
+ * Description: Longer description
  */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 /**
- * dog_t - new name for the type struct dog
+ * dog_t - typedef for struct dog
  */
 typedef struct dog dog_t;
 
-/* Function prototype */
-void init_dog(dog_t *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
-#endif /* DOG_H */
+#endif
